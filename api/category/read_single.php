4f57098d0c6a8d268a13b1bc40f758d09dcb9371
Kristina Botova <k_botova@mail.fhsu.edu>
2022-03-19 21:35:51 -0500

    Start of the project

---
+++ api/category/read_single.php
@@ -0,0 +1,29 @@
+<?php
+//Headers
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+//Instantiate DB and connect
+$database = new Database();
+$db = $database->connect();
+
+//Instantiate blog post object
+$category = new Category($db);
+
+//Get ID
+$category->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+//Get post
+$category->read_single();
+
+//Create array
+$category_arr = array(
+    'id' => $category->id,
+    'category' => $category->category,
+);
+
+//Make JSON
+print_r(json_encode($category_arr));
---
+++ index.php
@@ -0,0 +1,8 @@
+ <!DOCTYPE html>
+<html>
+<body>
+
+<h1>Kristina Botova</h1>
+
+</body>
+</html> 
